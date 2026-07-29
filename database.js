30cbd7961c25eea22fd0f2dfef7718ccca7d954c
homni203-a11y <homni203@gmail.com>
2026-07-29 13:07:54 +0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,136 @@
+/**
+ * CƠ SỞ DỮ LIỆU GEN POKEMON (100 POKEMON)
+ * System Update: Expanded DNA Sequence for Core Generator
+ */
+const PokemonDB = [
+    // --- GEN 1 (KANTO) ---
+    { id: 1, name: "BULBASAUR", types: ["Grass", "Poison"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" },
+    { id: 2, name: "IVYSAUR", types: ["Grass", "Poison"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" },
+    { id: 3, name: "VENUSAUR", types: ["Grass", "Poison"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
+    { id: 4, name: "CHARMANDER", types: ["Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" },
+    { id: 5, name: "CHARMELEON", types: ["Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png" },
+    { id: 6, name: "CHARIZARD", types: ["Fire", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" },
+    { id: 7, name: "SQUIRTLE", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" },
+    { id: 8, name: "WARTORTLE", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png" },
+    { id: 9, name: "BLASTOISE", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png" },
+    { id: 25, name: "PIKACHU", types: ["Electric"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" },
+    { id: 26, name: "RAICHU", types: ["Electric"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/26.png" },
+    { id: 38, name: "NINETALES", types: ["Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/38.png" },
+    { id: 54, name: "PSYDUCK", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/54.png" },
+    { id: 59, name: "ARCANINE", types: ["Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/59.png" },
+    { id: 65, name: "ALAKAZAM", types: ["Psychic"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/65.png" },
+    { id: 68, name: "MACHAMP", types: ["Fighting"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/68.png" },
+    { id: 79, name: "SLOWPOKE", types: ["Water", "Psychic"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/79.png" },
+    { id: 94, name: "GENGAR", types: ["Ghost", "Poison"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png" },
+    { id: 104, name: "CUBONE", types: ["Ground"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/104.png" },
+    { id: 123, name: "SCYTHER", types: ["Bug", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/123.png" },
+    { id: 130, name: "GYARADOS", types: ["Water", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png" },
+    { id: 131, name: "LAPRAS", types: ["Water", "Ice"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/131.png" },
+    { id: 133, name: "EEVEE", types: ["Normal"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png" },
+    { id: 134, name: "VAPOREON", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/134.png" },
+    { id: 135, name: "JOLTEON", types: ["Electric"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/135.png" },
+    { id: 136, name: "FLAREON", types: ["Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/136.png" },
+    { id: 143, name: "SNORLAX", types: ["Normal"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/143.png" },
+    { id: 144, name: "ARTICUNO", types: ["Ice", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/144.png" },
+    { id: 145, name: "ZAPDOS", types: ["Electric", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/145.png" },
+    { id: 146, name: "MOLTRES", types: ["Fire", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/146.png" },
+    { id: 149, name: "DRAGONITE", types: ["Dragon", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/149.png" },
+    { id: 150, name: "MEWTWO", types: ["Psychic"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png" },
+    { id: 151, name: "MEW", types: ["Psychic"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png" },
+
+    // --- GEN 2 (JOHTO) ---
+    { id: 152, name: "CHIKORITA", types: ["Grass"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png" },
+    { id: 155, name: "CYNDAQUIL", types: ["Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/155.png" },
+    { id: 158, name: "TOTODILE", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/158.png" },
+    { id: 172, name: "PICHU", types: ["Electric"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/172.png" },
+    { id: 175, name: "TOGEPI", types: ["Fairy"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/175.png" },
+    { id: 181, name: "AMPHAROS", types: ["Electric"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/181.png" },
+    { id: 196, name: "ESPEON", types: ["Psychic"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/196.png" },
+    { id: 197, name: "UMBREON", types: ["Dark"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/197.png" },
+    { id: 208, name: "STEELIX", types: ["Steel", "Ground"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/208.png" },
+    { id: 212, name: "SCIZOR", types: ["Bug", "Steel"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/212.png" },
+    { id: 214, name: "HERACROSS", types: ["Bug", "Fighting"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/214.png" },
+    { id: 227, name: "SKARMORY", types: ["Steel", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/227.png" },
+    { id: 229, name: "HOUNDOOM", types: ["Dark", "Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/229.png" },
+    { id: 243, name: "RAIKOU", types: ["Electric"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/243.png" },
+    { id: 244, name: "ENTEI", types: ["Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/244.png" },
+    { id: 245, name: "SUICUNE", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/245.png" },
+    { id: 248, name: "TYRANITAR", types: ["Rock", "Dark"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/248.png" },
+    { id: 249, name: "LUGIA", types: ["Psychic", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/249.png" },
+    { id: 250, name: "HO-OH", types: ["Fire", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/250.png" },
+
+    // --- GEN 3 (HOENN) ---
+    { id: 254, name: "SCEPTILE", types: ["Grass"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/254.png" },
+    { id: 257, name: "BLAZIKEN", types: ["Fire", "Fighting"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/257.png" },
+    { id: 260, name: "SWAMPERT", types: ["Water", "Ground"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/260.png" },
+    { id: 282, name: "GARDEVOIR", types: ["Psychic", "Fairy"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/282.png" },
+    { id: 302, name: "SABLEYE", types: ["Dark", "Ghost"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/302.png" },
+    { id: 330, name: "FLYGON", types: ["Ground", "Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/330.png" },
+    { id: 350, name: "MILOTIC", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/350.png" },
+    { id: 359, name: "ABSOL", types: ["Dark"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/359.png" },
+    { id: 373, name: "SALAMENCE", types: ["Dragon", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/373.png" },
+    { id: 376, name: "METAGROSS", types: ["Steel", "Psychic"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/376.png" },
+    { id: 380, name: "LATIAS", types: ["Dragon", "Psychic"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/380.png" },
+    { id: 381, name: "LATIOS", types: ["Dragon", "Psychic"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/381.png" },
+    { id: 382, name: "KYOGRE", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/382.png" },
+    { id: 383, name: "GROUDON", types: ["Ground"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/383.png" },
+    { id: 384, name: "RAYQUAZA", types: ["Dragon", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/384.png" },
+
+    // --- GEN 4 (SINNOH) ---
+    { id: 387, name: "TURTWIG", types: ["Grass"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/387.png" },
+    { id: 390, name: "CHIMCHAR", types: ["Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/390.png" },
+    { id: 393, name: "PIPLUP", types: ["Water"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/393.png" },
+    { id: 405, name: "LUXRAY", types: ["Electric"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/405.png" },
+    { id: 445, name: "GARCHOMP", types: ["Dragon", "Ground"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/445.png" },
+    { id: 448, name: "LUCARIO", types: ["Fighting", "Steel"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/448.png" },
+    { id: 461, name: "WEAVILE", types: ["Dark", "Ice"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/461.png" },
+    { id: 468, name: "TOGEKISS", types: ["Fairy", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/468.png" },
+    { id: 470, name: "LEAFEON", types: ["Grass"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/470.png" },
+    { id: 471, name: "GLACEON", types: ["Ice"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/471.png" },
+    { id: 475, name: "GALLADE", types: ["Psychic", "Fighting"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/475.png" },
+    { id: 483, name: "DIALGA", types: ["Steel", "Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/483.png" },
+    { id: 484, name: "PALKIA", types: ["Water", "Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/484.png" },
+    { id: 487, name: "GIRATINA", types: ["Ghost", "Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/487.png" },
+    { id: 493, name: "ARCEUS", types: ["Normal"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png" },
+
+    // --- GEN 5 (UNOVA) ---
+    { id: 494, name: "VICTINI", types: ["Psychic", "Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/494.png" },
+    { id: 571, name: "ZOROARK", types: ["Dark"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/571.png" },
+    { id: 609, name: "CHANDELURE", types: ["Ghost", "Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/609.png" },
+    { id: 612, name: "HAXORUS", types: ["Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/612.png" },
+    { id: 635, name: "HYDREIGON", types: ["Dark", "Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/635.png" },
+    { id: 638, name: "COBALION", types: ["Steel", "Fighting"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/638.png" },
+    { id: 643, name: "RESHIRAM", types: ["Dragon", "Fire"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/643.png" },
+    { id: 644, name: "ZEKROM", types: ["Dragon", "Electric"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/644.png" },
+    { id: 646, name: "KYUREM", types: ["Dragon", "Ice"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/646.png" },
+
+    // --- GEN 6 (KALOS) ---
+    { id: 658, name: "GRENINJA", types: ["Water", "Dark"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/658.png" },
+    { id: 681, name: "AEGISLASH", types: ["Steel", "Ghost"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/681.png" },
+    { id: 700, name: "SYLVEON", types: ["Fairy"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/700.png" },
+    { id: 715, name: "NOIVERN", types: ["Flying", "Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/715.png" },
+    { id: 716, name: "XERNEAS", types: ["Fairy"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/716.png" },
+    { id: 717, name: "YVELTAL", types: ["Dark", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/717.png" },
+
+    // --- GEN 7 (ALOLA) ---
+    { id: 722, name: "ROWLET", types: ["Grass", "Flying"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/722.png" },
+    { id: 724, name: "DECIDUEYE", types: ["Grass", "Ghost"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/724.png" },
+    { id: 727, name: "INCINEROAR", types: ["Fire", "Dark"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/727.png" },
+    { id: 778, name: "MIMIKYU", types: ["Ghost", "Fairy"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/778.png" },
+    { id: 791, name: "SOLGALEO", types: ["Psychic", "Steel"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/791.png" },
+    { id: 792, name: "LUNALA", types: ["Psychic", "Ghost"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/792.png" },
+
+    // --- GEN 8 (GALAR) ---
+    { id: 888, name: "ZACIAN", types: ["Fairy"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/888.png" },
+    { id: 889, name: "ZAMAZENTA", types: ["Fighting"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/889.png" },
+    { id: 890, name: "ETERNATUS", types: ["Poison", "Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/890.png" },
+
+    // --- GEN 9 (PALDEA) ---
+    { id: 906, name: "SPRIGATITO", types: ["Grass"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/906.png" },
+    { id: 1007, name: "KORAIDON", types: ["Fighting", "Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1007.png" },
+    { id: 1008, name: "MIRAIDON", types: ["Electric", "Dragon"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1008.png" },
+    { id: 1011, name: "OGERPON", types: ["Grass"], img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1011.png" }
+];
+
+// Hàm Get Random an toàn (Utility Function)
+const getRandomPokemon = () => PokemonDB[Math.floor(Math.random() * PokemonDB.length)];
